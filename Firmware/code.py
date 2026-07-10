e0e2ef8e4d2646dd5b0edda47b1be599dedf8221
Nadoooor <nadersayed742@gmail.com>
2026-07-11 00:40:34 +0300

    Added RGB support

--- Firmware/code.py
+++ Firmware/code.py
@@ -6,12 +6,23 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.modules.encoder import EncoderHandler
+from kmk.extensions.RGB import RGB, AnimationModes
 encoder_handler = EncoderHandler()
 
 keyboard.modules = [layers, holdtap, encoder_handler]
 
 keyboard = KMKKeyboard()
 
+rgb = RGB(
+    val_default=10,
+    val_limit=100,  # out of 255
+    pixel_pin=board.D6,
+    num_pixels=32,
+    refresh_rate=30,
+    animation_speed=3,
+    animation_mode=AnimationModes.STATIC,
+)
+keyboard.extensions.append(rgb)
 
 encoder_handler.pins = (
     (board.D10, board.D9, NONE)
