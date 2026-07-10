25e3fb0a188730ce4f02aea4e8224476217389a7
Nadoooor <nadersayed742@gmail.com>
2026-07-11 00:49:37 +0300

    Added OLED support

--- Firmware/code.py
+++ Firmware/code.py
@@ -1,15 +1,39 @@
 print("Starting")
 
 import board
+import busio
+import displayio
+import terminalio
+import adafruit_displayio_ssd1306
 
+from adafruit_display_text import label
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.modules.encoder import EncoderHandler
 from kmk.extensions.RGB import RGB, AnimationModes
 encoder_handler = EncoderHandler()
+from kmk.kmk_keyboard import KMKKeyboard
 
 keyboard.modules = [layers, holdtap, encoder_handler]
+
+i2c = busio.I2C(scl=board.SCL, sda=board.SDA)
+
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+display = adafruit_displayio_ssd1306.SSD1306_I2C(display_bus, width=128, height=64)
+
+text_area = label.Label(
+    terminalio.FONT, 
+    text="Made with LOVE, by @Nadoooor", 
+    color=0xFFFFFF, 
+    x=10, 
+    y=25
+)
+
+splash = displayio.Group()
+splash.append(text_area)
+display.show(splash)
+
 
 keyboard = KMKKeyboard()
 
@@ -36,6 +60,8 @@ keyboard.keymap = [
     [KC.F6, KC.F1, KC.ESC, KC.TRNS]
 ]
 encoder_handler.map = [ ((KC.VOLU, KC.VOLD, KC.NONE))
+
+
 
 if __name__ == '__main__':
     keyboard.go()
